0b140b237356f5b69a1d09c2d564f34653a62539
Koushik Das <sd2021667@gmail.com>
2022-12-28 03:35:01 +0530

    router added

--- index.js
+++ index.js
@@ -1,76 +1,16 @@
 const express = require('express');
 const http =  require('http');
-const fs = require('fs');
+const operations = require('./routes/operations');
 
 const HOST_NAME = 'localhost';
 const PORT = 3000;
-const PATH = __dirname + '/data/database.json';
 
 const app = express();
 app.use(express.json());
 
+app.use('/api', operations);
+
 const server = http.createServer(app);
-
-// Getting all users using endpoint /api/users
-app.get('/api/users', (req, res) => {
-    fs.readFile(PATH, 'utf8', (err, data) => {
-        if (err)
-            throw err;
-        res.send(data);
-    });
-});
-
-// Get user by Id using endpoint /api/:userId
-app.get('/api/user/:userId', (req, res) => {
-    fs.readFile(PATH, 'utf8', (err, data) => {
-        let users = JSON.parse(data);
-        res.send(users['user' + req.params.userId]);
-    });
-});
-
-/* POST using a JSON object in the format:
-    {
-        "user#id" : {
-            "name": "username",
-            "password": "pass",
-            "id": #id
-        }
-    }
-*/
-// Adding user into JSON database using endpoint /api/addUser
-app.post('/api/addUser', (req, res) => {
-    fs.readFile(PATH, 'utf8', (err, data) => {
-        if (err)
-            throw err;
-        
-        data = JSON.parse(data);
-        let newObj = req.body;
-        let newData = JSON.stringify({...data, ...newObj}, null, 4);
-        fs.writeFile(PATH, newData, (err) => {
-            if (err)
-                throw err;
-            res.send('New data added successfully:\n' + newData);
-        });
-    });
-});
-
-// Deleting a user using id at endpoint /api/delete/:userId
-app.delete('/api/delete/:userId', (req, res) => {
-    fs.readFile(PATH, 'utf8', (err, data) => {
-        if (err)
-            throw err;
-            
-        data = JSON.parse(data);
-        delete data['user' + req.params.userId];
-        fs.writeFile(PATH, JSON.stringify(data, null, 4), (err) => {
-            if (err)
-                throw err;
-            
-            res.send(data);
-        });
-    });
-});
-
 server.listen(PORT, HOST_NAME, () => {
     console.log(`Server running at http://${HOST_NAME}:${PORT}`);
 });
--- routes/operations.js
+++ routes/operations.js
@@ -1,7 +1,82 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const PATH =  __dirname + '/' + '../data/database.json';
 
-const router = express.Router();
+const operations = express.Router();
 
-router.use(bodyParser.json());
+operations.use(express.json());
 
+// Getting all users using endpoint /api/users
+operations.get('/users', (req, res) => {
+    // Reading from our JSON database/file
+    fs.readFile(PATH, 'utf8', (err, data) => {
+        // Check for errors
+        if (err)
+            throw err;
+        res.send(data);
+    });
+});
 
+// Get user by Id using endpoint /api/:userId
+operations.get('/user/:userId', (req, res) => {
+    // Reading from our JSON database/file
+    fs.readFile(PATH, 'utf8', (err, data) => {
+        // data is in string, so convert it to object using JSON.parse()
+        let users = JSON.parse(data);
+        // Send the file 
+        res.send(users['user' + req.params.userId]);
+    });
+});
+/* POST using a JSON object in the format:
+    {
+        "user#id" : {
+            "name": "username",
+            "password": "pass",
+            "id": #id
+        }
+    }
+*/
+// Adding user into JSON database using endpoint /api/addUser
+operations.post('/addUser', (req, res) => {
+    // Reading from our JSON database/file
+    fs.readFile(PATH, 'utf8', (err, data) => {
+        // Checking for errors
+        if (err)
+            throw err;
+        
+        // Parsing string data to object
+        data = JSON.parse(data);
+        let newObj = req.body;
+        // Merging data with the new object from request body
+        let newData = JSON.stringify({...data, ...newObj}, null, 4);
+        // Writing newData to the JSON database/file
+        fs.writeFile(PATH, newData, (err) => {
+            if (err)
+                throw err;
+            res.send('New data added successfully:\n' + newData);
+        });
+    });
+});
+
+// Deleting a user using id at endpoint /api/delete/:userId
+operations.delete('/delete/:userId', (req, res) => {
+    // Reading from our JSON database/file
+    fs.readFile(PATH, 'utf8', (err, data) => {
+        if (err)
+            throw err;
+            
+        // Parsing sting data into object
+        data = JSON.parse(data);
+        // Delete the object with the userId
+        delete data['user' + req.params.userId];
+        // Writing the changes to our JSON database/file
+        fs.writeFile(PATH, JSON.stringify(data, null, 4), (err) => {
+            if (err)
+                throw err;
+            res.send(data);
+        });
+    });
+});
+
+module.exports = operations;
