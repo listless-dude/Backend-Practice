ce52122a6a6a5559892e663c45463ff8b3a1e1ed
Koushik Das <sd2021667@gmail.com>
2022-12-28 03:05:58 +0530

    DELETE done

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ app.get('/api/users', (req, res) => {
     });
 });
 
-// Get user by Id using endpoint /api/:getUserId
-app.get('/api/:getUserId', (req, res) => {
+// Get user by Id using endpoint /api/:userId
+app.get('/api/user/:userId', (req, res) => {
     fs.readFile(PATH, 'utf8', (err, data) => {
         let users = JSON.parse(data);
-        
+        res.send(users['user' + req.params.userId]);
     });
 });
 
-/* POST requires a JSON in the format example
+/* POST using a JSON object in the format:
     {
         "user#id" : {
             "name": "username",
@@ -43,9 +43,9 @@ app.post('/api/addUser', (req, res) => {
         if (err)
             throw err;
         
-        let newObj = JSON.parse(data);
-        let newObj2 = req.body;
-        let newData = JSON.stringify({...newObj, ...newObj2});
+        data = JSON.parse(data);
+        let newObj = req.body;
+        let newData = JSON.stringify({...data, ...newObj}, null, 4);
         fs.writeFile(PATH, newData, (err) => {
             if (err)
                 throw err;
@@ -54,7 +54,22 @@ app.post('/api/addUser', (req, res) => {
     });
 });
 
-
+// Deleting a user using id at endpoint /api/delete/:userId
+app.delete('/api/delete/:userId', (req, res) => {
+    fs.readFile(PATH, 'utf8', (err, data) => {
+        if (err)
+            throw err;
+            
+        data = JSON.parse(data);
+        delete data['user' + req.params.userId];
+        fs.writeFile(PATH, JSON.stringify(data, null, 4), (err) => {
+            if (err)
+                throw err;
+            
+            res.send(data);
+        });
+    });
+});
 
 server.listen(PORT, HOST_NAME, () => {
     console.log(`Server running at http://${HOST_NAME}:${PORT}`);
